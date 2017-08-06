874ec7fb29ad898806437c460ef057af6b351ed4
esraa <esraa.alaarag@gmail.com>
2017-08-06 18:57:12 -0400

    here

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,16 @@ class Footer extends Component {
     render() {
         //each of the allergen icons
         const images = [
-            "/images/icons/egg-color.png",
-            "/images/icons/fish-color.png",
-            "/images/icons/milk-color.png",
-            "/images/icons/icons-blackandwhite/peanut-bw.png",
-            "/images/icons/sesame-color.png",
-            "/images/icons/shellfish-color.png",
-            "/images/icons/soy-color.png",
-            "/images/icons/treenut-color.png",
-            "/images/icons/wheat-color.png",
-            "/images/logo.png",
+            "/can_I_eat_this/images/icons/egg-color.png",
+            "/can_I_eat_this/images/icons/fish-color.png",
+            "/can_I_eat_this/images/icons/milk-color.png",
+            "/can_I_eat_this/images/icons/icons-blackandwhite/peanut-bw.png",
+            "/can_I_eat_this/images/icons/sesame-color.png",
+            "/can_I_eat_this/images/icons/shellfish-color.png",
+            "/can_I_eat_this/images/icons/soy-color.png",
+            "/can_I_eat_this/images/icons/treenut-color.png",
+            "/can_I_eat_this/images/icons/wheat-color.png",
+            "/can_I_eat_this/images/logo.png",
         ];
         //displays allergen icons on footer for fun!
         return (
